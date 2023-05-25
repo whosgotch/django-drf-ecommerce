621361033dccf3fd78a8f58650744ad5d14c009a
whosgotch <komovaar@Artems-MacBook-Air.local>
2023-05-25 17:07:55 +0300

    initial commit(django, drf, pytest)

---
+++ commands.md
@@ -0,0 +1,7 @@
+# Packages 
+
+djangorestframework==3.14.0
+Django==4.2.1
+python-dotenv==1.0.0 
+pytest==7.3.1
+pytest-django==4.5.2
---
+++ drfecommerce/drfecommerce/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
